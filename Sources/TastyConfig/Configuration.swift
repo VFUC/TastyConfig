81386817409f05dd77fa407519ccff461f67074c
Jonas Wippermann <dev@jonas.lol>
2020-12-05 14:16:11 +0100

    Updated feed to add a new field, test should pass

--- Sources/TastyConfig/Configuration.swift
+++ Sources/TastyConfig/Configuration.swift
@@ -6,7 +6,8 @@ import Foundation
 public struct Configuration: Codable {
 	public let identifier: String
 	let baseURL: String
-	// let feed: String
+	let feed: String
+	let user: String
 }
 
 extension Configuration {
@@ -23,14 +24,16 @@ extension Configuration {
 	/// This instance is used by ConfigMaker to generate the current version of the config feed
 	public static var production: Configuration {
 		Configuration(identifier: "production",
-					  baseURL: "https://api.my.app")
-//					  feed: "/feed")
+					  baseURL: "https://api.my.app",
+					  feed: "/feed",
+					  user: "/user")
 	}
 	
 	public static var stage: Configuration {
 		Configuration(identifier: "stage",
-					  baseURL: "https://stage.api.my.app")
-//					  feed: "/feed")
+					  baseURL: "https://stage.api.my.app",
+					  feed: "/feed",
+					  user: "/user")
 	}
 	
 	public static var all: [Configuration] {
